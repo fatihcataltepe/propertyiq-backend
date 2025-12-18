2530a3e8e09a0b3922b33743f16efe05cb3884d9
fatihcataltepe <fatihcataltepe@users.noreply.github.com>
2025-12-18 23:30:23 +0000

    Add common module with shared utilities

---
+++ common/src/main/java/com/propertyiq/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.propertyiq.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
