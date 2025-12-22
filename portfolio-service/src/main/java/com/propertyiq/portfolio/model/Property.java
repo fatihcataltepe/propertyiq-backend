7d05c47526c9347564c100eb00f21db3ad87dfe5
Fatih Cataltepe <fatihcataltepe@gmail.com>
2025-12-22 19:45:51 +0000

    schema portfolio_db

--- portfolio-service/src/main/java/com/propertyiq/portfolio/model/Property.java
+++ portfolio-service/src/main/java/com/propertyiq/portfolio/model/Property.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "properties")
+@Table(name = "properties", schema="portfolio_db")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
